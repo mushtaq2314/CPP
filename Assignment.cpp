9aa3faf25066bf84ebae18f414ba8fe4db4c884d
mushtaq2314 <115691632+mushtaq2314@users.noreply.github.com>
2022-10-19 21:50:54 +0530

    Add files via upload

---
+++ Assignment.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin>>a;
+    cin>>b;
+    if (a==b){
+        cout<<"Both are equal";
+    }
+    else{
+        cout<<"Both are not equal";
+    }
+    return 0;
+}
